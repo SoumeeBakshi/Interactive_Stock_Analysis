fcf5f2ac4085568a5762e8c8c4ce77ac25853497
SoumeeBakshi <soumeebakshi@gmail.com>
2026-09-12 18:06:38 +0530

    Add files via upload

---
+++ Trading_App.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Trading App",
+    page_icon = "chart_with_downwards_trend",
+    layout = "wide"
+)
+
+st.title("Trading Guide App :bar_chart:")
+st.header("We provide the Greatest platform for you to collect all information prior to investing in stocks.")
+
+st.image("TRADING_IMAGE.jpg", caption="Trading Dashboard", width=600)
+
+
+
+
+st.markdown("## We provide the following services:")
+
+st.markdown("#### :one: Stock Information")
+st.write("Through this page, you can see all the information about stock.")
+
+st.markdown("#### :two: CAPM Return")
+st.write("Discover how the Capital Asset Pricing Model (CAPM) calculates the expected return of different stocks asset based on its risk an...")
+
+
